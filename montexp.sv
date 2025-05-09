f638cc8555a2068ae0664d3cc19ddf9667f61841
fergus-xu <fergusx@gmail.com>
2025-05-09 10:33:35 -0700

    Update montexp.sv

--- montexp.sv
+++ montexp.sv
@@ -6,30 +6,31 @@ module montexp #(
     input logic rst,
     input logic start,
     input logic [WIDTH-1:0]  base,
-    input logic [EWIDTH-1:0] exponent
+    input logic [EWIDTH-1:0] exponent,
     input logic [WIDTH-1:0]  modulus,
     input logic [WIDTH-1:0]  n_prime,
+    input logic [WIDTH-1:0]  mont_one,
 
     output logic [WIDTH-1:0] result,
     output logic done
 )
 
     typedef enum logic [2:0] {
-        IDLE, INIT, SQUARE, MULTIPLY, UPDATE, FINISH
+        IDLE, INIT, MULTIPLY, WAIT_MULTIPLY, SQUARE, WAIT_SQUARE, FINISH
     } state_t;
     state_t state;
 
     // modular multiplication variables
-    logic [WIDTH-1:0] a, b, n_prime;
+    logic [WIDTH-1:0] a, b;
     logic [WIDTH-1:0] mult_result;
     logic mult_start, mult_done;
 
     montmult(.clk(clk), .rst(rst), .start(mult_start), .a(a), .b(b), .n(modulus), .n_prime(n_prime), .result(mult_result), .done(mult_done));
 
     logic [WIDTH-1:0] base_reg, result_reg;
-logic [EWIDTH-1:0] exp_reg;
+    logic [EWIDTH-1:0] exp_reg;
 
-always_ff @(posedge clk or posedge rst) begin
+    always_ff @(posedge clk or posedge rst) begin
     if (rst) begin
         state      <= IDLE;
         done       <= 0;
@@ -40,7 +41,7 @@ always_ff @(posedge clk or posedge rst) begin
                 done <= 0;
                 if (start) begin
                     base_reg   <= base;
-                    result_reg <= 1;    // R mod N if Montgomery, 1 otherwise
+                    result_reg <= mont_one;
                     exp_reg    <= exponent;
                     state      <= INIT;
                 end
@@ -50,35 +51,50 @@ always_ff @(posedge clk or posedge rst) begin
                 if (exp_reg == 0) begin
                     state <= FINISH;
                 end else if (exp_reg[0]) begin
-                    a         <= result_reg;
-                    b         <= base_reg;
-                    mult_start <= 1;
-                    state     <= MULTIPLY;
+                    state <= MULTIPLY;  // go to multiply
                 end else begin
-                    a         <= base_reg;
-                    b         <= base_reg;
-                    mult_start <= 1;
-                    state     <= SQUARE;
+                    state <= SQUARE;    // go to square
                 end
             end
 
             MULTIPLY: begin
+                mult_start <= 1;
+                a          <= result_reg;
+                b          <= base_reg;
+                state      <= WAIT_MULTIPLY;
+            end
+
+            WAIT_MULTIPLY: begin
                 if (mult_done) begin
                     result_reg <= mult_result;
                     mult_start <= 0;
-                    a          <= base_reg;
-                    b          <= base_reg;
-                    mult_start <= 1;
-                    state      <= SQUARE;
+                    
+                    if ((exp_reg >> 1) == 0) begin
+                        state <= FINISH;
+                    end else begin
+                        state <= SQUARE;
+                    end
                 end
             end
 
             SQUARE: begin
+                mult_start <= 1;
+                a          <= base_reg;
+                b          <= base_reg;
+                state      <= WAIT_SQUARE;
+            end
+
+            WAIT_SQUARE: begin
                 if (mult_done) begin
                     base_reg   <= mult_result;
                     exp_reg    <= exp_reg >> 1;
                     mult_start <= 0;
-                    state      <= INIT;
+
+                    if (exp_reg == 1) begin
+                        state <= FINISH;
+                    end else begin
+                        state <= INIT;
+                    end
                 end
             end
 
@@ -90,6 +106,5 @@ always_ff @(posedge clk or posedge rst) begin
         endcase
     end
 end
-
 
 endmodule
